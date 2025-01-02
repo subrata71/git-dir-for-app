7ae458f968059a54498af22c0949c196e59bc095
subrata <subrata@appsmith.com>
2025-01-02 07:11:12 +0000

    Add query, api, js object and table widget

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,16 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+		return 3232;
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	},
+	getNow() {
+		return moment.now();
+	}
+}
